eb076d199471e981226efd01f297793533d58a80
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2022-12-22 23:04:38 -0600

    it's alive... it's ALIVE

--- assets/script.js
+++ assets/script.js
@@ -15,40 +15,79 @@ generateBtn.addEventListener("click", writePassword);
 
 //variables
 var lowerCase =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-console.log(lowerCase);
-var upperCase =["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
-console.log(upperCase);
-var numberChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12','13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24,'];
-console.log(numberChars);
+var upperCase =["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numberChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
-console.log(specialCharacters);
 var inclLower;
 var inclUpper;
 var inclNum;
 var inclSpecial;
 
+var theseCharacters = [];
+var youChose = [];
 
-
-
-// var thisManyTimesTest = 8;
+//this function randomly picks variables out of the bucket for an array at some array.length
 function pickEm(arr, num){
   var answer = [];
   for (var i = 0; i < num; i++){
       answer.push(arr[Math.floor(Math.random()*arr.length)]);
-      console.log(answer);
+      // console.log(answer);
   };
  console.log(answer);
  return answer
 };
-// console.log(pickEm(lowerCase, thisManyTimesTest));
 
-  //   var compOption =["r", "p", "s"]
-  //   return compOption[Math.floor(Math.random()*3)];
+//this function verifies that elements from array1 "arr1" are in "arr2", making sure password includes lowerCase, upperCase, etc.
+function checkEm(arr1, arr2){
+  var pass = false;
+  for (var i = 0; i < arr1.length; i++){
+    if(arr2.includes(arr1[i])){
+      console.log("found character from " + arr1 + " in " + arr2 + " in " + (i + 1) + "tries!" );
+      pass = true;
+      break
+    }else{
+      continue
+    }
+  }
+  if (pass === false){
+    console.log("failed to find " + arr1 + " in " + arr2)
+    pickAndCheck(); //calls pickEm() and runs through checkEm() again
+  };
+};
+
+// checkEm test
+// var testArray = ['#', 'A', 'g', 'd', 'X', 'o', 'G', '~', '@', '%'];
+// checkEm(testArray, numberChars);
+// checkEm(testArray, specialCharacters);
+// checkEm(testArray, lowerCase);
+// checkEm(testArray, upperCase);
+
+//pickAndCheck() picks random characters with pickEm(), then checks that each selected character group is represented with checkEm(). If any of the checkEm's fail, checkEm() will start pickAndCheck() over again and pick a new answer.
+function pickAndCheck() {
+  answer = pickEm(theseCharacters, thisMany);
+  
+  if (inclLower){
+    checkEm(answer, lowerCase);
+  };
+  if (inclUpper){
+    checkEm(answer, upperCase);
+  };
+  if (inclNum){
+    checkEm(answer, numberChars);
+  };
+  if (inclSpecial){
+    checkEm(answer, specialCharacters);
+  };
+  console.log("all good in the hood!");
+  return answer
+};
+
+var thisMany = 0;
 
 function generatePassword() {
   //must return a string value that is the password
   //user is prompted for pw length between 8 and 128
-var thisMany;
+thisMany = 0; //reset before user input
 var howMany = window.prompt("I'll help you make up a gobbledygook password.\nHow many characters do you want?\nPick a number between 8 and 128.");
 console.log(howMany)
 if (isNaN(howMany)){
@@ -59,21 +98,21 @@ if (isNaN(howMany)){
     window.alert("OK, I'm gonna say this again real slow. I want a number BIGGER than 8, but SMALLER than 128. There are at least 131 correct answers if you include the 8 and the 128, which I do.\nPick one.");
     generatePassword();
 } else {
-  (howMany)*1
-  if (Number.isInteger(howMany)){
-            console.log("howMany an int? " + Number.isInteger(howMany))
-            thisMany = howMany;
+  var thatMany = (howMany)/1
+  if (Number.isInteger(thatMany)){
+            console.log("howMany an int? " + Number.isInteger(thatMany))
+            thisMany = thatMany;
             console.log(thisMany);
         } else {  
           thisMany = Math.round(howMany);
-          window.alert("Jeez Einstein. No reason to make it difficult. I rounded " + howMany + " to " + thisMany +".\n You're welcome");
+          window.alert("Jeez Einstein. No reason to make it difficult. I rounded " + howMany + " to " + thisMany +".\nYou're welcome.");
         };
     };
   };
 
-
-window.alert("OK, one password with " + thisMany + " characters comin' right up!\n Now I got some y/n questions for yous. That means you can type 'y' for 'yes' or 'n' for 'no.'");
-// function charTypes(){
+window.alert("OK, one password with " + thisMany + " characters. No problem.\n But first I got some y/n questions for yous. That means you can type 'y' for 'yes' or 'n' for 'no.'");
+// these functions prompt the user for what kind of characters they want to include
+//they also offer unique insults to the user if the user doesn't give the correct input, and then start over.
 function incLA() {
     inclLower = window.prompt("Do you want some lowercase letters in that password? y/n")
 if (inclLower == 'y'){
@@ -137,52 +176,48 @@ if (inclSpecial == 'y'){
   incSpeci();
 };
 }; incSpeci();
+//if the user chooses none of the available variable types, this insults the user and includes only lowercase letters
 if (!inclLower && !inclUpper && !inclNum && !inclSpecial){
     window.alert("So you're tellin' me that you want a password with no lowercase letters, no uppercase letters, no numbers, and no special characters? Exactly HOW you want me to do that? Ain't nothin else to type!" );
     window.alert("Listen, I'll keep it simple for ya. You're getting a password with only lowercase letters.");
     inclLower = true;
     console.log("inclLower is " + inclLower);
 };
-var theseCharacters = [];
-var youChose = [];
+//builds the bucket of characters to choose from
+
+theseCharacters = []; //resets these arrays
+youChose = [];
+
 if (inclLower){
-    theseCharacters.concat(lowerCase);
+    theseCharacters = theseCharacters.concat(lowerCase);
     youChose.push("lowercase Characters");
-    console.log(theseCharacters);
+    // console.log(theseCharacters);
     };
 if (inclUpper){
-    theseCharacters.concat(upperCase);
+    theseCharacters = theseCharacters.concat(upperCase);
     youChose.push("UPPERCASE Characters");
-    console.log(theseCharacters);
+    // console.log(theseCharacters);
     };
 if (inclNum){
-    theseCharacters.concat(numberChars);
+    theseCharacters = theseCharacters.concat(numberChars);
     youChose.push("Numbers");
-    console.log(theseCharacters);
+    // console.log(theseCharacters);
     };
 if (inclSpecial){
-    theseCharacters.concat(specialCharacters);
+    theseCharacters = theseCharacters.concat(specialCharacters);
     youChose.push("Special Characters");
-    console.log(theseCharacters);
+    // console.log(theseCharacters);
     };
 
 window.alert("One password with " + thisMany + " characters, including: " + JSON.stringify(youChose) + " coming right up.");
-window.alert("the theseCharacters array looks like " + JSON.stringify(theseCharacters) );
+// window.alert("the theseCharacters array looks like " + JSON.stringify(theseCharacters) );
 
-console.log(pickEm(theseCharacters, thisMany));
+stringThis = pickAndCheck();
+console.log(stringThis);
 
-// }
-// charTypes();
-    //var to save length
-  // validate user num
-  //confirm whether or not to include lowercase, uppercase, numeric, special characters (4 confirm prompts)
-    //4 vars to save T/F of included categories
-    //validate that at least one character type is included with for loop
-        //how to decide how many elements to choose from each array?
-    // random math function getRandomInt(max) {
-  //   var compOption =["r", "p", "s"]
-  //   return compOption[Math.floor(Math.random()*3)];
-  // } 
-    //join chosen elements into string
-//     return passwordString
-}
+//makes array into a string
+
+passwordString = stringThis.join('');
+window.alert("Enjoy your password you goofy '" + passwordString + "'!");
+return passwordString
+};
